198b9fa457e5962b93420616d6bbd4ece65fb570
SubodhaSahu <104969063+SubodhaSahu@users.noreply.github.com>
2022-08-10 18:22:21 +0530

    Delete capitalize.js

--- _js/capitalize.js
+++
@@ -1,7 +0,0 @@
-function capitalize(str) {
-
-    return typeof str === "string" ? 
-            str.charAt(0).toUpperCase().concat(str.substring(1, str.length)) : str;
-}
-
-module.exports = capitalize
